a77ffe7280e412c8c404e5ff1fc6b8203f7277d3
Skoler <logosevens@gmail.com>
2023-10-12 17:59:43 +0900

    Create quote handler added

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,61 @@
-use axum::http;
+use axum::{extract::State, http, Json};
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+
+#[derive(Serialize)]
+pub struct Quote {
+    id: uuid::Uuid,
+    book: String,
+    quote: String,
+    created_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+impl Quote {
+    fn new(book: String, quote: String) -> Self {
+        let now = chrono::Utc::now();
+        Self {
+            id: uuid::Uuid::new_v4(),
+            book,
+            quote,
+            created_at: now,
+            updated_at: now,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateQuote {
+    book: String,
+    quote: String,
+}
 
 pub async fn health() -> http::StatusCode {
     http::StatusCode::OK
+}
+
+pub async fn create_quote(
+    State(pool): State<PgPool>,
+    Json(payload): Json<CreateQuote>,
+) -> Result<(http::StatusCode, Json<Quote>), http::StatusCode> {
+    let quote = Quote::new(payload.book, payload.quote);
+
+    let res = sqlx::query(
+        r#"
+        INSERT INTO quotes (id, book quote, created_at, updated_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+    )
+    .bind(&quote.id)
+    .bind(&quote.book)
+    .bind(&quote.quote)
+    .bind(&quote.created_at)
+    .bind(&quote.updated_at)
+    .execute(&pool)
+    .await;
+
+    match res {
+        Ok(_) => Ok((http::StatusCode::CREATED, Json(quote))),
+        Err(_) => Err(http::StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 mod handlers;
-use axum::routing::{get, Router};
-use handlers::health;
+use axum::routing::{get, post, Router};
+use handlers::{create_quote, health};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    let app = Router::new().route("/", get(health));
+    let database_url = env::var("DATABASE_URL").expect("missing DATABASE_URL env");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
+    let app = Router::new()
+        .route("/", get(health))
+        .route("/quotes", post(create_quote))
+        .with_state(pool);
 
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
